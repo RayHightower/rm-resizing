48df4e4da54075f186ff157c4671e8517a3be38f
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-17 10:41:12 -0500

    review the available method signatures

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,7 +2,7 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
-    @window.backgroundColor = UIColor.redColor
+    @window.UIColor.backgroundColor = UIColor.redColor
 
     first_view_controller = MyButtonController.alloc.initWithNibName(nil, bundle: nil)
     @window.rootViewController = first_view_controller
